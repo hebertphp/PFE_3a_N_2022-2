906e82d6f3cafc09557ecdbb90a6c32c90b4e56c
Hebert <hebertphp@gmail.com>
2022-11-01 20:49:03 -0300

    01/11/22

--- aula5/script5_01-2.js
+++ aula5/script5_01-2.js
@@ -1,8 +1,11 @@
 let msg;//declaração da variável msg
 const saida=document.querySelector(".saida");//class="saida"
+const h1=document.querySelector('h1');//nome de tag <h1>
 
 msg="Hoje a aula mudou!";
 
 saida.innerHTML=`${msg}`;
 saida.innerHTML+=`<br>Mesmo!!`;
 saida.innerHTML+=`<br>${msg}`;
+
+h1.innerHTML="Javascript";

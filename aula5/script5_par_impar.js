3b8426df575b2c3956c6d91bb5054a953bcfda11
Hebert <hebertphp@gmail.com>
2022-11-01 21:43:43 -0300

    01/11/22

---
+++ aula5/script5_par_impar.js
@@ -0,0 +1,14 @@
+/*
+Digitar um número inteiro (prompt) / parseInt
+verificar se o número é PAR ou ÍMPAR %2 resto da divisão por 2
+Mostrar o número digitar e mensagem PAR ou ÍMPAR
+*/
+const saida=document.querySelector(".saida");
+let numero=parseInt(prompt("Digite um numero"));
+
+if(numero%2 ==0){
+    saida.innerHTML=`${numero} - PAR`;
+}
+else{
+    saida.innerHTML=`${numero} - ÍMPAR`;
+}

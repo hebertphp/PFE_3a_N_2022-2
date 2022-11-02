b18e2f4ec9913c27e7925ab8477f391d21f57784
Hebert <hebertphp@gmail.com>
2022-11-01 21:58:19 -0300

    01/11/22

---
+++ aula5/script5_desafio.js
@@ -0,0 +1,7 @@
+/*
+Digitar duas notas (av1 e av2)
+Calcular média das notas (av1 e av2)
+media=(av1+av2)/2
+Com o valor da média, mostrar e verificar se o aluno está Aprovado ou Reprovado
+Condição para ser aprovado, media >=6
+*/
--- aula5/script5_par_impar.js
+++ aula5/script5_par_impar.js
@@ -3,7 +3,7 @@ Digitar um número inteiro (prompt) / parseInt
 verificar se o número é PAR ou ÍMPAR %2 resto da divisão por 2
 Mostrar o número digitar e mensagem PAR ou ÍMPAR
 */
-const saida=document.querySelector(".saida");
+const saida=document.querySelector(".saida");//class="saida"
 let numero=parseInt(prompt("Digite um numero"));
 
 if(numero%2 ==0){

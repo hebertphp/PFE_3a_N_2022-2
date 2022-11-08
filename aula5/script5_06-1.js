bb0c075e6c473e7cf638c5d01edd60dc87502818
Hebert <hebertphp@gmail.com>
2022-11-08 20:28:51 -0300

    08/11/22

---
+++ aula5/script5_06-1.js
@@ -0,0 +1,17 @@
+let a,b;
+const saida=document.querySelector(".saida");
+a=6;
+b=5;
+if (a == b) {
+ saida.innerHTML="A variável a é igual a variável b";
+}
+else if (a < b){
+ saida.innerHTML="A variável a é menor que a variável b";
+}
+else{
+ saida.innerHTML="A variável a é maior que a variável b";
+}
+/*
+
+
+*/

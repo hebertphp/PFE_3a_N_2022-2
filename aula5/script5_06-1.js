ac3d97a8bd9cb0296f89520ceb9671c519c4aed4
Hebert <hebertphp@gmail.com>
2022-11-08 20:45:23 -0300

    08/11/22

--- aula5/script5_06-1.js
+++ aula5/script5_06-1.js
@@ -12,6 +12,31 @@ else{
  saida.innerHTML="A variável a é maior que a variável b";
 }
 /*
+se tiver 2 situaçãoes como fica?
+if(){}
+else{}
+
+se tiver 3 situçãoes?
+if(){ }
+else if()
+else { }
+
+Se tiver 7 situações para analisar, 
+como ficam as instruções if , else if, else
+if(){ }
+else if(){}
+else if(){}
+else if(){}
+else if(){}
+else if(){}
+else{ }
+
+Se tiver 4 situações
+if(){}
+else if(){}
+else if(){}
+else{}
+
 
 
 */

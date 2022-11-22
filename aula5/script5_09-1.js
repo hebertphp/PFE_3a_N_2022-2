a2a6e7046d7e6ae2d2db34b638a491429036272d
Hebert <hebertphp@gmail.com>
2022-11-22 20:40:46 -0300

    22/11/22

--- aula5/script5_09-1.js
+++ aula5/script5_09-1.js
@@ -1,7 +1,19 @@
-let i;
-const saida=document.querySelector(".saida");
-i=0;
+let i;//declaração de var i
+const saida=document.querySelector(".saida");//referência de uma class="saida"
+i=0;//var i recebe o valor 0, valor inicial
 do{
     saida.innerHTML+=i + "<br>";
-    i++;
-}while (i < 3);
+    i++;//somar 1
+}while (i < 3); //condição
+saida.innerHTML+="<hr>";
+i=0;
+while(i<3){
+    saida.innerHTML+=i + "<br>";
+    i++;//somar 1
+}
+
+/*
+0
+1
+2
+*/

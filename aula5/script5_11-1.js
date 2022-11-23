6462e50231c458525a85f2663d2095d29940b47c
Hebert <hebertphp@gmail.com>
2022-11-22 21:55:38 -0300

    22/11/22

--- aula5/script5_11-1.js
+++ aula5/script5_11-1.js
@@ -1,10 +1,24 @@
-let i,j;
+let i,j,k;
 const saida=document.querySelector(".saida");
-for (i=0; i<3; i++) {
-    for (j=0; j<4; j++) {
-        saida.innerHTML+=`${i} ${j}<br>`;
+for (i=0; i<3; i++) {//looping externo hora
+    for (j=0; j<4; j++){//looping interno minuto
+        for(k=0;k<2;k++){//segundo
+            saida.innerHTML+=`${i} ${j} ${k}<br>`;
+        }
     }
 }
 /*
-
+i   j
+0   0
+0   1
+0   2
+0   3
+1   0
+1   1
+1   2
+1   3
+2   0
+2   1
+2   2
+2   3
 */
---
+++ aula5/script5_11-2.js
@@ -0,0 +1,19 @@
+/*
+2 x 0 = 0
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+*/
+let tabuada, i;
+const saida=document.querySelector(".saida");
+tabuada=2;
+for(i=0;i<=10;i++){
+    saida.innerHTML+=`${tabuada} x ${i} = ${tabuada*i}<br>`;
+}

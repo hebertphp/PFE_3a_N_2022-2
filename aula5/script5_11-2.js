69fce34856ee95985c5a1e2a4b0f273eb74b0ed1
Hebert <hebertphp@gmail.com>
2022-11-22 22:00:01 -0300

    22/11/22

--- aula5/script5_11-2.js
+++ aula5/script5_11-2.js
@@ -13,7 +13,9 @@
 */
 let tabuada, i;
 const saida=document.querySelector(".saida");
-tabuada=2;
-for(i=0;i<=10;i++){
-    saida.innerHTML+=`${tabuada} x ${i} = ${tabuada*i}<br>`;
+for(tabuada=0;tabuada<=10;tabuada++){
+    for(i=0;i<=10;i++){
+        saida.innerHTML+=`${tabuada} x ${i} = ${tabuada*i}<br>`;
+    }
+    saida.innerHTML+=`<hr>`;
 }

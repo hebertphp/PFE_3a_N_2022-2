573f9f518486e958524f3467fdcaf3fd458b9ca5
Hebert <hebertphp@gmail.com>
2022-11-22 21:11:19 -0300

    22/11/22

--- aula5/script5_10-1.js
+++ aula5/script5_10-1.js
@@ -1,5 +1,12 @@
 let i;
 const saida=document.querySelector(".saida");
+//   VI   cond  contador
 for (i=0; i<6; i++) {
    saida.innerHTML+=i + " ";
 }
+/*
+i++ contador somando 1
+i+=2 contador somando 2
+0 1 2 3 4 5
+*/
+//console.log("sei lá");

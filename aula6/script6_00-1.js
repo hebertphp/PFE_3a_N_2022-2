87fa526692b9f3b46861c8065d3b3891eb1c908e
Hebert <hebertphp@gmail.com>
2022-12-06 20:31:17 -0300

    06/12/22

--- aula6/script6_00-1.js
+++ aula6/script6_00-1.js
@@ -1,4 +1,6 @@
 const saida=document.querySelector(".saida");
-let nomes=["Merida","Gabriel","Daniel","Bianca"];
+//para criar um array, let nome_do_array=[]; const nome_do_array=[];
+// indice ou posição      0       1           2        3         
+            let nomes=["Merida","Gabriel","Daniel","Bianca"];
 
-saida.innerHTML=nomes;
+saida.innerHTML=nomes[3];

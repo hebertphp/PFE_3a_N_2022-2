8c7fd33d9a10cfcc4b7571b0c83b37c4fe1a7abf
Hebert <hebertphp@gmail.com>
2022-12-06 20:39:03 -0300

    06/12/22

--- aula6/script6_00-1.js
+++ aula6/script6_00-1.js
@@ -3,4 +3,11 @@ const saida=document.querySelector(".saida");
 // indice ou posição      0       1           2        3         
             let nomes=["Merida","Gabriel","Daniel","Bianca"];
 
-saida.innerHTML=nomes[3];
+saida.innerHTML=`${nomes[0]}<br>`;
+saida.innerHTML+=`${nomes[1]}<br>`;
+saida.innerHTML+=`${nomes[2]}<br>`;
+saida.innerHTML+=`${nomes[3]}<br>`;
+/*
+[⬆][Alt][setinha para baixo]--serve para clonar a linha
+[Ctrl][Alt[setinha para baixio]--cria um multi cursor
+*/

2018a0ff92a00f93d2eb676383126acbf5af0d26
Hebert <hebertphp@gmail.com>
2022-12-06 22:00:26 -0300

    06/12/22

--- aula6/script6_07-1.js
+++ aula6/script6_07-1.js
@@ -1,7 +1,8 @@
 const saida=document.querySelector('.saida');
-const estudante=['Astrogildo','Belarmina','Pafuncia','Claudionora'];
+const estudante=['Astrogildo','Belarmina','Pafuncia'];
 //for(let var_simples of array)
 //pegar todos os valores, um de cada vez, e atribui para a var_simples
 for (let x of estudante){
     saida.innerHTML+=`${x}<br>`;
 }
+// [Ctrl][;] comentário

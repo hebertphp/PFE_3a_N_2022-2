9b3cce8dea1a712b7a5b2f5a1ad592900e66e994
Hebert <hebertphp@gmail.com>
2022-12-13 21:50:02 -0300

    13/12/22

--- aula7/aula7_02.js
+++ aula7/aula7_02.js
@@ -2,8 +2,21 @@ const saida=document.querySelector('.saida');
 const nome=document.querySelector('#nome');
 const av1=document.querySelector('#av1');
 const av2=document.querySelector('#av2');
+const btn1=document.querySelector('.btn1');
+const btn2=document.querySelector('.btn2');
 
+btn1.onclick=calc;
+btn1.onclick=boas_vindas("Deoclesia");
+
+btn2.addEventListener("click", calc);
+btn2.addEventListener("click", boas_vindas("Deoclesia"));
 function calc(){
-
-    saida.innerHTML=`Funciona??`
+    let media=(parseFloat(av1.value)+parseFloat(av2.value))/2;
+    saida.innerHTML=`Nome: ${nome.value}<br>`;
+    saida.innerHTML+=`AV1: ${av1.value}<br>`;
+    saida.innerHTML+=`AV2: ${av2.value}<br>`;
+    saida.innerHTML+=`Media: ${media}<br>`;
+}
+function boas_vindas(nome) {//criação da função
+    saida.innerHTML+=`Seja bem vindo(a) ${nome}!<br>`;
 }

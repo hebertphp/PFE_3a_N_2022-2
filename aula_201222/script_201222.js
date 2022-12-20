0cf70d4d9619e369594e3f2446a45660e0e56b20
Prof. Hebert <hebertphp@gmail.com>
2022-12-20 20:34:02 -0300

    20/12/22

--- aula_201222/script_201222.js
+++ aula_201222/script_201222.js
@@ -1,9 +1,16 @@
 const saida=document.querySelector('.saida');
-
+//             0          1         2         3 
 const nomes=["Lucas", "Daniela","Aderbal","Timóteo"];
+//const estudante=["Aderbal",123456789,"TADS",1];
+const aluno={
+    nome:"Aderbal",
+    ra:123456789,
+    curso:"TADS",
+    semestre:1
+};
 
-saida.innerHTML=nomes;
-
+// saida.innerHTML=nomes[2];//Aderbal
+saida.innerHTML=aluno;
 
 /*
 https://www.w3schools.com/js/js_objects.asp

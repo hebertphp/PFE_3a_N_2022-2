c6c35be30d35a778a7cb564bccf4487d3f5e14d0
Prof. Hebert <hebertphp@gmail.com>
2022-12-20 21:23:27 -0300

    20/12/22

--- aula_201222/script_201222.js
+++ aula_201222/script_201222.js
@@ -2,6 +2,8 @@ const saida=document.querySelector('.saida');
 //             0          1         2         3 
 const nomes=["Lucas", "Daniela","Aderbal","Timóteo"];
 //const estudante=["Aderbal",123456789,"TADS",1];
+//const nome=[valor, valor]; é um array
+//const nome={propriedade:"valor_string",propriedade:valor}; é um objeto
 const aluno={
     nome:"Aderbal",
     ra:123456789,
@@ -13,13 +15,23 @@ const aluno={
 saida.innerHTML=aluno.nome+"<br>";
 saida.innerHTML+=aluno.ra+"<br>";
 saida.innerHTML+=aluno.curso+"<br>";
-saida.innerHTML+=aluno.semestre+"<br>";
+saida.innerHTML+=aluno.semestre+"<hr>";
 console.log(aluno);//comando de saída e direciona para o console
 
+const presente={
+    nome:"PS5",
+    jogo:"God of war",
+    valor:5000,
+    tipo:"🎁"
+}
+saida.innerHTML+=presente.nome+"<br>";
+saida.innerHTML+=presente.jogo+"<br>";
+saida.innerHTML+=presente.valor+"<br>";
+saida.innerHTML+=presente.tipo+"<br>";
 /*
 Desafio:
 Criar um objeto presente_natal
-com 3 propriedades, sendo uma delas o valor do presente(R$)
+com pelo menos 3 propriedades, sendo uma delas o valor do presente(R$)
 Mostrar os dados na página
 */
 
